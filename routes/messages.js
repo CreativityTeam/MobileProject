14ef61e25a55513203a6761ce54e35d3365692c6
Nhat Nguyen Developer <nhatndm1193@gmail.com>
2016-11-01 01:03:13 +0700

    Message Model is Done
    Message Model is Done

--- routes/messages.js
+++ routes/messages.js
@@ -42,11 +42,45 @@ function getMesSender(id, callback) {
     });
 };
 
-router.get('/getMessageSender/:token',function(req,res){
-    var token = req.params.token;
-    var decoded = jwt.decode(token,configAuth.secret);
-    var listMessage = getMesSender(decoded._id);
-    getMesSender(decoded._id,function(listMessage){
+function getMesReceiver(id, callback) {
+    var listMessage = [{
+        idReceiver: String,
+        nameSender: String,
+        content: String,
+        date: Date
+    }];
+    Message.getMessageReceiver(id, function (err, messageList) {
+        if (err) throw err;
+        var listQuery = [];
+        for (var i in messageList) {
+            listQuery.push(
+                new Promise(function(resolve,reject){
+                    User.getUserById(messageList[i].receiverid,function(err,User){
+                        if(err) reject(err);
+                        var message = {
+                            idReceiver: id,
+                            nameSender: User.local.name,
+                            content: messageList.content,
+                            date: messageList.time_created   
+                        };
+                        resolve(message);
+                    });   
+                })
+            );
+        }
+        Promise.all(listQuery)
+        .then(function(messageArray){
+            listMessage.push(messageArray);
+            callback(listMessage);
+        })
+        .catch(function (err){
+            console.log(err);
+        });
+    });
+};
+
+router.get('/getMessSender/:id',function(req,res){
+    getMesSender(req.params.id,function(listMessage){
         if (listMessage.length == 0) {
             res.json({
                 success: false,
@@ -62,4 +96,52 @@ router.get('/getMessageSender/:token',function(req,res){
     });
 });
 
+router.get('/getMessReceiver/:id',function(req,res){
+    getMesReceiver(req.params.id,function(listMessage){
+        if (listMessage.length == 0) {
+            res.json({
+                success: false,
+                msg: "You dont have any mails",
+            });
+        } else {
+            res.json({
+                success: true,
+                msg: "Get Done",
+                data: listMessage
+            });
+        }
+    });
+});
+
+router.post('/insertMess',function(req,res){
+    var idSender = req.body.idsender;//*Get Id Sender
+    var idReceiver = req.body.idreceiver;
+    var content = req.body.content;
+    var currentDate = new Date();
+    var newDate = new Date(currentDate.getTime() - (currentDate.getTimezoneOffset()*60000));
+    var newMessage = new Message({
+        senderid : idSender,
+        receiverid : idReceiver,
+        content : content,
+        time_created : newDate
+    });
+    Message.addMessage(newMessage,function(err,message){
+        if(err) throw err;
+        res.json({
+            success : true,
+            msg : "Successfully Send Mail",
+            data : message
+        });
+    });
+});
+
+router.delete('/deleteMess/:id',function(req,res){
+    Message.deleteMessage(req.params.id,function(err){
+        if(err) throw err;
+        res.json({
+            success : true,
+            msg : "Successfully Delete"
+        });
+    });
+});
 module.exports = router;
