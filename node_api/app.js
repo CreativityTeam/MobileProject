c705aecde6b9b7db395a42dfbd9240001c44ffa4
trongnghianguyen1994 <trongnghianguyen1994@gmail.com>
2016-11-01 18:35:56 +0700

    Complete Food, Comment, Order Model APIs

--- node_api/app.js
+++ node_api/app.js
@@ -76,6 +76,9 @@ var users = require('./routes/users');
 var restaurants = require('./routes/restaurants');
 var ratings = require('./routes/ratings');
 var photos = require('./routes/photos');
+var foods = require('./routes/foods');
+var comments = require('./routes/comments');
+var orders = require('./routes/orders');
 
 /**URL for model */
 //* Its purpose to call the right api for model
@@ -83,6 +86,9 @@ app.use('/api/users',users);
 app.use('/api/restaurants',restaurants);
 app.use('/api/ratings',ratings);
 app.use('/api/photos',photos);
+app.use('/api/comments',comments);
+app.use('/api/foods',foods);
+app.use('/api/orders',orders);
 
 /**Set up Server */
 app.listen(port,function(){
